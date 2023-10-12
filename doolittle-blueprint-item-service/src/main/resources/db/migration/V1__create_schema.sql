2fb40e817ea1227ec685c2e309926c7559c54cc4
jayeshjagtap14 <jayesh.jagtap@learningmate.com>
2023-10-12 10:47:36 +0530

    Schema updated

--- doolittle-blueprint-item-service/src/main/resources/db/migration/V1__create_schema.sql
+++ doolittle-blueprint-item-service/src/main/resources/db/migration/V1__create_schema.sql
@@ -8,15 +8,11 @@ CREATE TABLE `content` (
   `created_at` bigint(20) DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `language` varchar(5) DEFAULT NULL,
-  `processing_state` varchar(16) DEFAULT NULL,
-  `processing_detail` longtext DEFAULT NULL,
   `attribution` json DEFAULT NULL,
-  `raw_source` VARCHAR(255) DEFAULT NULL,
   `tags` json DEFAULT NULL,
   `labeled_version` int DEFAULT NULL,
   `comment` longtext DEFAULT NULL,
-  `content_components` json DEFAULT NULL,
-  `layout` json DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_contentlist_content_content_id` (`content_id`, `id`)
+  `previewUrl` varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+  KEY `idx_blueprintitem_content_content_id` (`content_id`, `id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
